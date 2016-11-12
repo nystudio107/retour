4b89da759072a8496ee45c82258dc4debc3ad946
Tim Kelty <tim@fusionary.com>
2016-11-12 17:28:04 -0500

    Control via config

--- config.php
+++ config.php
@@ -15,4 +15,6 @@ return array(
  * Controls whether Retour automatically creates static redirects when an entry's URI changes.
  */
     "createStaticRedirects" => true,
+    "staticRedirectDisplayLimit" => 100,
+    "dynamicRedirectDisplayLimit" => 100,
 );
